522f1bc92eee02e0a2a182f3dd45ec24cdeab520
baba-s <shotababa1206@gmail.com>
2022-09-10 11:12:44 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Kogane.DebugLogMethodChainingExtensionMethods
+# Kogane Debug Log Method Chaining Extension Methods
+
+Debug.Log をメソッドチェーンで書けるようにする拡張メソッド
+
+## 使用例
+
+```csharp
+public string GetDisplayName()
+{
+    var id   = 25;
+    var name = "ピカチュウ";
+
+    return $"{id}: {name}";
+}
+```
+
+たとえば上記のような関数で戻り値の値をログ出力したい場合、
+
+```csharp
+public string GetDisplayName()
+{
+    var id   = 25;
+    var name = "ピカチュウ";
+
+    var result = $"{id}: {name}";
+    
+    Debug.Log( result );
+
+    return result;
+}
+```
+
+通常であればこのようにローカル変数を定義する必要がありますが
+
+```csharp
+public string GetDisplayName()
+{
+    var id   = 25;
+    var name = "ピカチュウ";
+
+    return $"{id}: {name}".DebugLog();
+}
+```
+
+拡張メソッドを使えばローカル変数の定義が不要になります  
---
+++ Runtime/DebugLogMethodChainingExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+    public static class DebugLogMethodChainingExtensionMethods
+    {
+        public delegate void LogDelegate( object item );
+
+        public static LogDelegate OnLog        { get; set; } = item => Debug.Log( item );
+        public static LogDelegate OnLogWarning { get; set; } = item => Debug.LogWarning( item );
+        public static LogDelegate OnLogError   { get; set; } = item => Debug.LogError( item );
+
+        public static T DebugLog<T>( this T self )
+        {
+            OnLog?.Invoke( self );
+            return self;
+        }
+
+        public static T DebugLogWarning<T>( this T self )
+        {
+            OnLogWarning?.Invoke( self );
+            return self;
+        }
+
+        public static T DebugLogError<T>( this T self )
+        {
+            OnLogError?.Invoke( self );
+            return self;
+        }
+    }
+}
